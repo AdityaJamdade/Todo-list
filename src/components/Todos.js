f85e09386ecc68e5a92c9e84490edbc1a77905be
AdityaJamdade <adijamdade156@gmail.com>
2022-11-07 09:33:43 +0530

    added update functionality at frontend

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import React from 'react'
-import Todo from './Todo'
 
 const Todos = () => {
 
@@ -9,18 +8,28 @@ const Todos = () => {
   const [todos, setTodos] = useState([])
 
   useEffect(() => {
+    // eslint-disable-next-line
     getTodos();
   }, [])
   const [todo, setTodo] = useState({ title: "" });
+  const [updatedTodo, setUpdatedTodo] = useState({ updatedtitle: "" });
 
+  const ref = useRef(null);
+  const refClose = useRef(null);
   const handleClick = (e) => {
     e.preventDefault();
     console.log(todo);
     addTodo(todo);
   }
 
-  const handleSubmit = () => {
+  const handleSaveChanges = () => {
+    refClose.current.click();
+  }
 
+  const handleUpdate = (todo) => {
+    console.log(todo);
+    ref.current.click();
+    editTodo(todo);
   }
 
   const handleOnChange = (e) => {
@@ -29,8 +38,8 @@ const Todos = () => {
 
   const getTodos = async () => {
     // api call for fetching todos
-    const response = await fetch(`${host}/api/fetchalltodos` , {
-      method:'GET',
+    const response = await fetch(`${host}/api/fetchalltodos`, {
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json'
       }
@@ -43,12 +52,12 @@ const Todos = () => {
 
   const addTodo = async (todo) => {
     // api call for adding todo
-    const response = await fetch(`${host}/api/addtodo` , {
-      method:'POST',
+    const response = await fetch(`${host}/api/addtodo`, {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({title:todo.title})
+      body: JSON.stringify({ title: todo.title })
     })
 
     const newTodo = await response.json();
@@ -58,8 +67,8 @@ const Todos = () => {
 
   const deleteTodo = async (todo) => {
     // api call for deleting todo
-    const response = await fetch(`${host}/api/deletetodo/${todo._id}` , {
-      method:'DELETE',
+    const response = await fetch(`${host}/api/deletetodo/${todo._id}`, {
+      method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
       },
@@ -69,55 +78,103 @@ const Todos = () => {
 
     console.log("deleting", todo);
     const toDelete = todo;
-    setTodos(todos.filter((todo) => ( todo._id !== toDelete._id )));
+    setTodos(todos.filter((todo) => (todo._id !== toDelete._id)));
   }
 
-  const editNote = async (todo, title) => {
+  const editTodo = async (todo, title) => {
     // api call for updating todo
-    const response = await fetch(`${host}/api/updatetodo/${todo._id}` , {
-      method:'PUT',
+    const response = await fetch(`${host}/api/updatetodo/${todo._id}`, {
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({title})
+      body: JSON.stringify({ title })
     })
     const json = response.json();
 
+    // let newTodos = JSON.parse(json.stringify(todos))
     for (let index = 0; index < todos.length; index++) {
       const element = todos[index];
-      if(element._id === todo._id){
-        element.title = title;
+      if (element._id === todo._id) {
+        todos[index].title = title;
+        break;
       }
     }
+    setTodos(todos);
   }
+
 
   return (
     <>
+      {/* <!-- Button trigger modal --> */}
+      <button ref={ref} type="button" className="btn btn-primary d-none" data-toggle="modal" data-target="#exampleModal">
+        Launch demo modal
+      </button>
+
+      {/* <!-- Modal --> */}
+      <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+            <form>
+                <div className="form-group row">
+                  <label htmlFor="updatedTitle" className="col-sm-2 col-form-label">Task</label>
+                  <div className="col-sm-10 d-flex">
+                    <input type="text" className="form-control" id='updatedTitle' name='updatedTitle' value={updatedTodo.title} onChange={handleOnChange} />
+                    <button className="btn btn-primary mx-1" onClick={handleClick}>Add</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="row m-3">
         <h2>Yout Todos</h2>
-        <div className="row">
-          {todos.map((todo) => {
-            return (<div key={todo._id} className="col-md-4 my-2">
+        <div className="row w-full">
+          {todos && todos.map((todo) => {
+            return (<div key={todo._id} className="col-md-3 my-2">
               <div className="card">
                 <div className="card-body">
                   <h5 className="card-title ">{todo.title}</h5>
                   <i className="fa-solid fa-trash mx-2" onClick={() => { deleteTodo(todo) }}></i>
-                  <i className="fa-solid fa-edit mx-2"></i>
+                  <i className="fa-solid fa-edit mx-2" onClick={() => { handleUpdate(todo) }}></i>
                 </div>
               </div>
             </div>)
           })}
         </div>
-      </div>
-      <div className="col-md-3 m-4">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title my-2">Create new task here!</h5>
-            <form action="" onSubmit={handleSubmit}>
-              <input type="text" placeholder='Add Todo' className='d-block my-2 mt-3 form-control' name="title" id="title" onChange={handleOnChange} value={todo.title} />
-              <button type='submit' className='btn btn-primary' onClick={handleClick}>Add</button>
-            </form>
-
+        <div className="my-2" style={{ "width": "500px" }}>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title ">Add new</h5>
+              <form>
+                <div className="form-group row">
+                  <label htmlFor="title" className="col-sm-2 col-form-label">Task</label>
+                  <div className="col-sm-10 d-flex">
+                    <input type="text" className="form-control" id='title' name='title' value={todo.title} onChange={handleOnChange} />
+                    <button className="btn btn-primary mx-1" onClick={handleClick}>Add</button>
+                  </div>
+                </div>
+                {/* <div className="form-group row">
+                <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
+                <div className="col-sm-10">
+                  <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
+                </div>
+              </div> */}
+              </form>
+            </div>
           </div>
         </div>
       </div>
